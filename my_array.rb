028ef26b2d67fa37c64d41d7028f17e47c0e2e1f
taki <taki@users.noreply.github.com>
2021-01-15 09:09:08 +0900

    Implement more methods

--- my_array.rb
+++ my_array.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 class MyArray
+  include Enumerable
+
+  private_class_method :new
+
+  class << self
+    def [](*element)
+      @element = element
+    end
+  end
+
+  def [](i)
+    @element[i]
+  end
+
+  def each(&block)
+    return to_enum unless block_given?
+
+    @element.each(&block)
+
+    self
+  end
 end
--- spec/my_array_spec.rb
+++ spec/my_array_spec.rb
@@ -3,5 +3,36 @@
 require_relative '../my_array'
 
 RSpec.describe MyArray do
-  it { expect(described_class.new.is_a?(described_class)).to eq true }
+  describe '.new' do
+    it { expect { described_class.new }.to raise_error(NoMethodError) }
+  end
+
+  describe '#[]' do
+    let(:my_array) { described_class[0, 1, 2] }
+
+    it { expect(my_array[0]).to eq 0 }
+    it { expect(my_array[1]).to eq 1 }
+    it { expect(my_array[2]).to eq 2 }
+    it { expect(my_array[3]).to eq nil }
+  end
+
+  describe '#each' do
+    let(:my_array) { described_class[0, 1, 2] }
+
+    context 'when block is given' do
+      specify do
+        return_value = my_array.each {}
+
+        expect(return_value).to eq my_array
+      end
+    end
+
+    context 'when block is not given' do
+      specify do
+        return_value = my_array.each
+
+        expect(return_value).to be_an Enumerator
+      end
+    end
+  end
 end
